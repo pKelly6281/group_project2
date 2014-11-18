f82b610d556b994767f4fc3940b7d726962f4439
Patrick Kelly <pk6281@mcla.edu>
2014-11-18 14:03:57 -0500

    Finished I think

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'hashmaps' =>'main_controller#hashmaps'
 
-  get 'sparsematrix' =>'main_controller/sparsematrix'
+  get 'sparsematrix' =>'main_controller#sparsematrix'
 
   root 'main_controller#home'
 end
